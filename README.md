9212b1c3b9c9b928665c0e923f0ab7eb0c7b72d8
Evan Kaufman <evan@digitalflophouse.com>
2015-09-29 14:52:16 -0500

    Added purge-kernels script

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# ubuntu-purge-kernels
-Remove all but the running and two installed installed kernels for an Ubuntu server
+# Why is this necessary?
+
+In most cases, it isn't -- running `apt-get autoremove` should identify and remove old packages (including old kernels).
+
+# Then _when_ is this necessary?
+
+Ubuntu's package manager has to read from and write to the `/boot` directory in order to autoremove old kernels, and if there is insufficient space to do this, the package manager will fail with rather cryptic error messages.
+
+The sure fire remedy is to carefully find and remove the old kernels by hand and then update the boot loader, [as explained in this Stack Exchange post](http://askubuntu.com/questions/345588/what-is-the-safest-way-to-clean-up-boot-partition#answer-430944).
+
+Instead, you can run the included python script (requires root or sudo permission). By default, it will short circuit unless the `/boot` directory is 75% or more full:
+
+```
+root@localhost# ./purge-kernels.py
+$ df -k /boot
+
+$ du -ks /boot
+
+Boot partition is only 74% full
+```
+
+You can override this by passing a minimum percentage as an argument:
+
+```
+root@localhost# ./purge-kernels.py 50
+$ df -k /boot
+
+$ du -ks /boot
+
+$ dpkg --list "linux-image*"
+
+$ uname -r
+
+Found: 3.13.0-57
+
+Found: 3.13.0-58
+
+Found: 3.13.0-59
+
+$ ls -1 /boot/*-3.13.0-57-*
+
+Removing:
+/boot/abi-3.13.0-57-generic
+/boot/config-3.13.0-57-generic
+/boot/initrd.img-3.13.0-57-generic
+/boot/System.map-3.13.0-57-generic
+/boot/vmlinuz-3.13.0-57-generic
+
+$ rm -rf /boot/*-3.13.0-57-*
+
+$ update-grub
+
+```
+
---
+++ purge-kernels.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python2
+
+# See: http://askubuntu.com/questions/345588/what-is-the-safest-way-to-clean-up-boot-partition#answer-430944
+
+import re
+import sys
+import subprocess
+
+'''Print and execute a system command'''
+def call(command, stderr=True):
+    print "$ %s\n" % command
+    output = subprocess.check_output(command, stderr=subprocess.STDOUT if stderr else None, shell=True, universal_newlines=True)
+    return output
+
+# determine free space on boot
+disk_space = call('df -k /boot')
+matched = re.search('^\S+[\t ]+(\d+)\s+(?:\d+\s+){2}(\d+)%\s+(\/[\S]*)$', disk_space, re.M)
+
+if not matched:
+    raise SystemExit('Could not determine free space on /boot:\n\n%s' % disk_space)
+
+# if /boot is its own partition, save ourselves some calculations
+if matched.group(3) == '/boot':
+    free_percent = int(matched.group(2))
+# otherwise, determine /boot size as % of containing partition
+else:
+    part_size = matched.group(1)
+    boot_space = call('du -ks /boot')
+    matched = re.search('(\d+)\s+\/boot$', boot_space)
+    free_percent = ( int(part_size) / int(matched.group(1)) ) / 100
+
+# bail unless over X% full (75 by default)
+min_percent = 75
+if len(sys.argv) > 1:
+    try:
+        min_percent = int(sys.argv[1])
+    except ValueError:
+        pass
+
+if free_percent < min_percent:
+    raise SystemExit('Boot partition is only %s%% full' % free_percent)
+
+# list all kernels
+all_kernels = call('dpkg --list "linux-image*"')
+
+# get running kernel
+running_kernel = call('uname -r')
+
+# de-dupe kernel versions by assigning as dict keys
+remove_kernels = dict()
+for line in all_kernels.split("\n"):
+    # match only installed kernels
+    matched = re.search('^ii\s+linux-image-[a-z-]*([\d.]+-\d+)-.*?$', line)
+    if matched:
+        # skip running kernel
+        if matched.group(1) not in running_kernel:
+            remove_kernels[ matched.group(1) ] = True
+
+# sort kernels to remove (as segmented versions)
+remove_kernels = remove_kernels.keys()
+remove_kernels.sort(key=lambda s: map(int, re.split('[.-]', s)))
+
+for version in remove_kernels:
+    print 'Found: %s\n' % version
+
+# if more than two, exclude the last two
+if len(remove_kernels) > 2:
+    del remove_kernels[-2:]
+
+# remove all files for given versions on /boot
+was_removed = False
+for version in remove_kernels:
+    # find & print any existing files for version
+    try:
+        to_be_removed = call('ls -1 /boot/*-%s-*' % version)
+        print 'Removing:\n%s' % to_be_removed
+    # fail gracefully if none found
+    except subprocess.CalledProcessError:
+        print 'No files to remove for %s' % version
+    # otherwise remove them with extreme prejudice
+    else:
+        call('rm -rf /boot/*-%s-*' % version)
+        was_removed = True
+
+# rebuild grub boot configurations
+if was_removed:
+    call('update-grub')
